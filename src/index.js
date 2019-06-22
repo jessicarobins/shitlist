132ad190bd4b10cc899d30ec21338aca66e55b14
Jessica Robins <jess@getupside.com>
2019-06-21 21:59:08 -0400

    add redux too

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, compose } from 'redux';
+import { reactReduxFirebase } from 'react-redux-firebase';
+import { Provider } from 'react-redux';
 
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from 'firebase/app';
-
 // Add the Firebase products that you want to use
 import 'firebase/auth';
 import 'firebase/firestore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers';
+import firebaseConfig from './config/firebase';
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig) // firebase instance as first argument
+  // reduxFirestore(firebase) // <- needed if using firestore
+)(createStore);
+
+// Create store with reducers and initial state
+const initialState = {};
+const store = createStoreWithFirebase(rootReducer, initialState);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
